a9782eae7554987b9ff507727d0d26ba975e7534
stujbrown <stuart.j.brown.90@gmail.com>
2021-12-01 09:25:32 +0000

    Day 1 solution

--- Day1/Program.cs
+++ Day1/Program.cs
@@ -1,14 +1,38 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
+﻿var depths = File.ReadAllLines("input.txt").Select(str => int.Parse(str)).ToArray();
 
-namespace Aoc
+Part1(depths);
+Part2(depths);
+
+
+void Part1(int[] depths)
 {
-    public class Program
+    int measurementIncreases = 0;
+    for (int i = 1; i < depths.Length; ++i)
     {
-        public static void Main(string[] args)
+        if (depths[i] - depths[i - 1] > 0)
         {
-            Console.WriteLine("Hello World!");
+            ++measurementIncreases;
         }
     }
+
+    Console.WriteLine($"{measurementIncreases} standard increases");
 }
+
+void Part2(int[] depths)
+{
+    int measurementIncreases = 0;
+    int lastSum = depths.Take(3).Sum();
+    for (int i = 1; i < depths.Length; ++i)
+    {
+        int sum = depths.Take(new Range(i, i + 3)).Sum();
+        int depthDiff = sum - lastSum;
+        if (sum > lastSum)
+        {
+            ++measurementIncreases;
+        }
+        lastSum = sum;
+    }
+
+    Console.WriteLine($"{measurementIncreases} sliding increases");
+}
+
