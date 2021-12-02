e0f9eca8dc0692b72a5272cb5a530fb73ea1c70f
stujbrown <stuart.j.brown.90@gmail.com>
2021-12-02 11:15:58 +0000

    Day 2 complete

---
+++ Day2/Program.cs
@@ -0,0 +1,36 @@
+﻿var instructions = File.ReadAllLines("input.txt").Select(str =>
+{
+    var split = str.Split(' ');
+    return (split[0], int.Parse(split[1]));
+}).ToArray();
+
+int ProcessInstructions(bool useAim)
+{
+    int horizontalPosition = 0;
+    int depth = 0;
+    int aim = 0;
+
+    foreach (var instruction in instructions)
+    {
+        if (useAim)
+        {
+            if (instruction.Item1.Equals("forward"))
+            {
+                horizontalPosition += instruction.Item2;
+                depth += aim * instruction.Item2;
+            }
+            else if (instruction.Item1.Equals("down")) aim += instruction.Item2;
+            else if (instruction.Item1.Equals("up")) aim -= instruction.Item2;
+        }
+        else
+        {
+            if (instruction.Item1.Equals("forward")) horizontalPosition += instruction.Item2;
+            else if (instruction.Item1.Equals("down")) depth += instruction.Item2;
+            else if (instruction.Item1.Equals("up")) depth -= instruction.Item2;
+        }
+    }
+    return horizontalPosition * depth;
+}
+
+Console.WriteLine($"Final position without aim: {ProcessInstructions(useAim: false)}");
+Console.WriteLine($"Final position with aim: {ProcessInstructions(useAim: true)}");
