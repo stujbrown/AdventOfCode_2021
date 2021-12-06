5c899aeb9edd2318fff9fc0b6cc55ba1f9b1ec8a
stujbrown <stuart.j.brown.90@gmail.com>
2021-12-06 12:41:20 +0000

    Day6

---
+++ Day6/Program.cs
@@ -0,0 +1,27 @@
+﻿var originalLifetimes = File.ReadAllLines("input.txt")[0].Split(',').Select(x => int.Parse(x));
+
+Console.WriteLine($"Total fish after 80 days: {CountFishAfterDays(80)}");
+Console.WriteLine($"Total fish after 256 days: {CountFishAfterDays(256)}");
+
+Int64 CountFishAfterDays(int numDays)
+{
+    var numFishPerInterval = new Int64[9];
+    foreach (var fish in originalLifetimes)
+    {
+        ++numFishPerInterval[fish];
+    }
+
+    int dayOffset = 0;
+    for (int day = 0; day < numDays; ++day)
+    {
+        Int64 numFishToday = numFishPerInterval[dayOffset];
+        numFishPerInterval[dayOffset] -= numFishToday;
+
+        dayOffset = (dayOffset + 1) % numFishPerInterval.Length;
+        numFishPerInterval[(8 + dayOffset) % numFishPerInterval.Length] += numFishToday;
+        numFishPerInterval[(6 + dayOffset) % numFishPerInterval.Length] += numFishToday;
+
+
+    }
+    return numFishPerInterval.Sum();
+}
