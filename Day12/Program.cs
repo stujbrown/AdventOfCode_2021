0decdc035b1a7df0f791338f46d3e6ce22ae8a5d
stujbrown <stuart.j.brown.90@gmail.com>
2021-12-12 18:15:17 +0000

    Day 12 simplification so I can compare vs other solutions

--- Day12/Program.cs
+++ Day12/Program.cs
@@ -1,35 +1,35 @@
 ﻿var connections = File.ReadAllLines("input.txt").Select(connection => connection.Split('-'));
-var nodeLookup = new Dictionary<string, Node>();
-
-foreach (var connection in connections)
-{
-    Node GetNode(string name)
-    {
-        if (!nodeLookup.TryGetValue(name, out var node))
-        {
-            node = new Node { Name = name };
-            nodeLookup[name] = node;
-        }
-        return node;
-    };
-
-    Node node0 = GetNode(connection[0]);
-    Node node1 = GetNode(connection[1]);
-    node0.Links.Add(node1);
-    node1.Links.Add(node0);
-}
-
-var start = nodeLookup["start"];
-var end = nodeLookup["end"];
 
 Solve(allowSmallDoubleVisit: false);
 Solve(allowSmallDoubleVisit: true);
 
 void Solve(bool allowSmallDoubleVisit)
 {
-    var visitCounts = new Dictionary<Node, int>();
+    var nodeLookup = new Dictionary<string, Node>();
+
+    foreach (var connection in connections)
+    {
+        Node GetNode(string name)
+        {
+            if (!nodeLookup.TryGetValue(name, out var node))
+            {
+                node = new Node { Name = name };
+                nodeLookup[name] = node;
+            }
+            return node;
+        };
+
+        Node node0 = GetNode(connection[0]);
+        Node node1 = GetNode(connection[1]);
+        node0.Links.Add(node1);
+        node1.Links.Add(node0);
+    }
+
+    var start = nodeLookup["start"];
+    var end = nodeLookup["end"];
+
     var stack = new Stack<Node>();
-    visitCounts.Add(start, 1);
+    start.Visited++;
 
     int routeCount = 0;
     bool doubleVisitCount = !allowSmallDoubleVisit;
@@ -40,7 +40,7 @@ void Solve(bool allowSmallDoubleVisit)
 
     void StepDown(Node node)
     {
-        if (node.Name == "end")
+        if (node == end)
         {
             ++routeCount;
         }
@@ -50,35 +50,26 @@ void Solve(bool allowSmallDoubleVisit)
 
             foreach (var link in node.Links)
             {
-                bool isSpecial = link.Name == "end" || link.Name == "start";
+                bool isSpecial = link == end || link == start;
                 bool isSmallCave = Char.IsLower(link.Name[0]) && !isSpecial;
 
-                int visitCount = 0;
-                visitCounts.TryGetValue(link, out visitCount);
+                int visitCount = link.Visited;
 
                 if (visitCount == 0 || (!isSmallCave && !isSpecial) || (!doubleVisitCount && visitCount < 2 && isSmallCave))
                 {
-                    visitCounts[link] = visitCount + 1;
                     if (visitCount > 0 && isSmallCave)
                     {
                         doubleVisitCount = true;
                     }
+                    link.Visited++;
 
                     StepDown(link);
 
-                    visitCount = visitCounts[link];
-                    if (visitCount == 1)
+                    if (link.Visited > 1 && isSmallCave)
                     {
-                        visitCounts.Remove(link);
+                        doubleVisitCount = false;
                     }
-                    else
-                    {
-                        visitCounts[link] = visitCount - 1;
-                        if (isSmallCave)
-                        {
-                            doubleVisitCount = false;
-                        }
-                    }
+                    link.Visited--;
                 }
             }
             stack.Pop();
@@ -89,5 +80,6 @@ void Solve(bool allowSmallDoubleVisit)
 class Node
 {
     public string Name;
+    public int Visited;
     public List<Node> Links = new List<Node>();
 };
